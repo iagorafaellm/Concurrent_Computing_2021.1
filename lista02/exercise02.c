1d86086be7f2475bda59865740aef43004f3921b
Iago Rafael Lucas Martins <iagorafaellm@gmail.com>
2021-09-27 19:36:15 -0300

    lista02/exercise02 initial commit

---
+++ lista02/exercise02.c
@@ -0,0 +1,9 @@
+/* Disciplina: Computação Concorrente - 2021.1 */
+/* Prof.: Silvana Rossetto */
+/* Estudante: Iago Rafael Lucas Martins */
+/* Segunda lista de exercícios - Exercício 02 */
+/* Código: projetar e implementar uma aplicação concorrente em Java usando o padrão básico leitores/escritores. */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
